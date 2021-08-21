c4c5ade65a72a54268f09e3b197dc7384ea028e2
italo Garcia <lobo123445@outlook.es>
2021-08-21 03:13:45 -0600

    ejercisio de clase

---
+++ ejercisio32/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercisio
+{
+    public class Class1
+    {
+        
+
+        public static void sortedListColletion()
+        {
+
+            double c;
+            double RESUL;
+            string linea;
+            Console.Write("grados ºC : "); linea = Console.ReadLine();
+            c = int.Parse(linea);
+            RESUL = (c * 9 / 5) + 32;
+            Console.WriteLine();
+            Console.WriteLine("El Resultado es " + RESUL + "ºF");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ejercisio32/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ejercisio;
+
+namespace ejercisio32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.sortedListColletion();
+            Console.ReadKey();
+
+        }
+    }
+}
